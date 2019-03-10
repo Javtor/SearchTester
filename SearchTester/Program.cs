9e02e43400b8ed7e9c0976a695ff7a212871edd8
Javier Torres <javierandrestorresreyes@gmail.com>
2019-03-10 18:48:39 -0500

    Console menu

---
+++ SearchTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            do
+            {
+                userInput = DisplayMenu();
+            } while (userInput != 3);
+        }
+
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Search Tester");
+            Console.WriteLine();
+            Console.WriteLine("1. Add a document");
+            Console.WriteLine("2. Query");
+            Console.WriteLine("3. Exit");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+    }
+}
